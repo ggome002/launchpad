6fcd9e1d13b68e0e13f50b364886832b0fcece49
Adonis Hernandez <adonhern@fiu.edu>
2023-12-15 10:02:36 -0500

    Catalog search and filter buttons

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ console.log($('div[stacked_graph] span'));
 
 arr = $('div[stacked_graph] span')
 arr.each(function (elem) {
-    console.log(this);
     $(this).width($(this).attr('data-percent')+'%');
 });
+
+
+$('div[filter] span').click(function(e){
+    $('div[filter] span').removeAttr('active');
+    $(this).attr('active','');
+})
+
 
